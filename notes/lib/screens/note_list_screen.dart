cee363f3296bb4f93064134d7d9d25b650aad68f
sasaakuma <saputridevita1@gmail.com>
2024-05-07 11:36:59 +0700

    update notes

--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -1,17 +1,18 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:notes/note_dialog.dart';
 import 'package:notes/services/note_service.dart';
+import 'package:notes/note_dialog.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
+
 
   @override
   State<NoteListScreen> createState() => _NoteListScreenState();
 }
 
+
 class _NoteListScreenState extends State<NoteListScreen> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,80 +25,21 @@ class _NoteListScreenState extends State<NoteListScreen> {
           showDialog(
             context: context,
             builder: (context) {
-              return AlertDialog(
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Add'),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Text(
-                        'Title : ',
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                    TextField(
-                      controller: _titleController,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Text(
-                        'Description : ',
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                    TextField(
-                      controller: _descriptionController,
-                    ),
-                  ],
-                ),
-                actions: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(); //Pop untuk menutup
-                        },
-                        child: const Text('Cancel')),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      noteService
-                          .addNote(_titleController.text,
-                              _descriptionController.text)
-                          .whenComplete(() {
-                        _titleController.clear();
-                        _descriptionController.clear();
-                        Navigator.of(context).pop();
-                      });
-
-                      // Map<String, dynamic> newNote = {};
-                      // newNote['Title'] = _titleController.text;
-                      // newNote['Description'] = _descriptionController.text;
-
-                      // FirebaseFirestore.instance
-                      //     .collection('notes')
-                      //     .add(newNote)
-                      //     .whenComplete(() {
-                      //   Navigator.of(context).pop();
-                      // });
-                    },
-                    child: const Text('Save'),
-                  ),
-                ],
-              );
+              return NoteDialog();
             },
           );
         },
-        tooltip: 'All Notes',
+        tooltip: 'Add Note',
         child: const Icon(Icons.add),
       ),
     );
   }
 }
 
+
 class NoteList extends StatelessWidget {
   const NoteList({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -105,7 +47,7 @@ class NoteList extends StatelessWidget {
       stream: noteService.getNoteList(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return Text('Error : ${snapshot.error}');
+          return Text('Error: ${snapshot.error}');
         }
         switch (snapshot.connectionState) {
           case ConnectionState.waiting:
@@ -117,165 +59,32 @@ class NoteList extends StatelessWidget {
               padding: const EdgeInsets.only(bottom: 80),
               children: snapshot.data!.map((document) {
                 return Card(
-                    child: ListTile(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        TextEditingController titleController =
-                            TextEditingController(text: document['Title']);
-                        TextEditingController descriptionController =
-                            TextEditingController(
-                                text: document['Description']);
-                        return AlertDialog(
-                          title: const Text(
-                            'Update Notes',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize:
-                                  25, // Sesuaikan dengan ukuran yang Anda inginkan
-                            ),
-                          ),
-                          content: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Title : ',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize:
-                                      18, // Sesuaikan dengan ukuran yang Anda inginkan
-                                ),
-                                textAlign: TextAlign.start,
-                              ),
-                              TextField(
-                                controller: titleController,
-                                // decoration: InputDecoration(
-                                //   hintText: document['Title'],
-                                // ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(top: 8.0),
-                                child: Text(
-                                  'Description : ',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize:
-                                        18, // Sesuaikan dengan ukuran yang Anda inginkan
-                                  ),
-                                  textAlign: TextAlign.start,
-                                ),
-                              ),
-                              TextField(
-                                controller: descriptionController,
-                                // decoration: InputDecoration(
-                                //   hintText: document['Description'],
-                                // ),
-                              ),
-                            ],
-                          ),
-                          actions: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pop(); //Pop untuk menutup
-                                  },
-                                  child: const Text('Cancel')),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                noteService
-                                    .updateNote(
-                                        document['id'],
-                                        titleController.text,
-                                        descriptionController.text)
-                                    .whenComplete(
-                                        () => Navigator.of(context).pop());
-
-                                // Map<String, dynamic> updateNote = {};
-                                // updateNote['Title'] = titleController.text;
-                                // updateNote['Description'] =
-                                //     descriptionController.text;
-
-                                // FirebaseFirestore.instance
-                                //     .collection('notes')
-                                //     .doc(document.id)
-                                //     .update(updateNote)
-                                //     .whenComplete(() {
-                                //   Navigator.of(context).pop();
-                                // });
-                              },
-                              child: const Text('Update'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  title: Text(document['Title']),
-                  subtitle: Text(document['Description']),
-                  trailing: InkWell(
+                  child: ListTile(
                     onTap: () {
-                      noteService.deleteNote(document['id']);
-                      // _showDeleteConfirmationDialog(context, document.id);
-                      // FirebaseFirestore.instance
-                      //     .collection('notes')
-                      //     .doc(document.id)
-                      //     .delete()
-                      //     .catchError((e) {
-                      //   print(e);
-                      // });
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return NoteDialog(note: document);
+                        },
+                      );
                     },
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: Icon(Icons.delete),
+                    title: Text(document['title']),
+                    subtitle: Text(document['description']),
+                    trailing: InkWell(
+                      onTap: () {
+                        noteService.deleteNote(document['id']);
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Icon(Icons.delete),
+                      ),
                     ),
                   ),
-                ));
+                );
               }).toList(),
             );
         }
       },
     );
   }
-}
-
-void _showDeleteConfirmationDialog(BuildContext context, String documentId) {
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Konfirmasi'),
-          content: Text('Apakah Anda yakin ingin menghapus item ini ?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Tidak'),
-            ),
-            TextButton(
-              onPressed: () {
-                _deleteItem(documentId);
-                Navigator.of(context).pop();
-              },
-              child: Text('Ya'),
-            ),
-          ],
-        );
-      });
-}
-
-void _deleteItem(String documentId) {
-  FirebaseFirestore.instance
-      .collection('notes')
-      .doc(documentId)
-      .delete()
-      .catchError((e) {
-    print(e);
-  });
 }
---
+++ notes/lib/widgets/note_dialog.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:notes/services/note_service.dart';
+
+
+class NoteDialog extends StatelessWidget {
+  final Map<String, dynamic>? note;
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+
+  NoteDialog({super.key, this.note}) {
+    if (note != null) {
+      _titleController.text = note!['title'];
+      _descriptionController.text = note!['description'];
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(note == null ? 'Add Notes' : 'Update Notes'),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Title: ',
+            textAlign: TextAlign.start,
+          ),
+          TextField(
+            controller: _titleController,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              'Description: ',
+            ),
+          ),
+          TextField(
+            controller: _descriptionController,
+          ),
+        ],
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (note == null) {
+              noteService.addNote(
+                      _titleController.text, _descriptionController.text)
+                  .whenComplete(() {
+                Navigator.of(context).pop();
+              });
+            } else {
+              noteService.updateNote(note!['id'], _titleController.text,
+                      _descriptionController.text)
+                  .whenComplete(() => Navigator.of(context).pop());
+            }
+          },
+          child: Text(note == null ? 'Add' : 'Update'),
+        ),
+      ],
+    );
+  }
+}
