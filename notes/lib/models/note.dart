d355b6a0d48a5caa099917c5fa19f9cbf3975657
sasaakuma <129216591+sasaakuma@users.noreply.github.com>
2024-05-21 09:17:16 +0700

    update error

--- notes/lib/models/note.dart
+++ notes/lib/models/note.dart
@@ -1,141 +1,47 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:notes/models/note.dart';
-import 'package:notes/note_service.dart';
-import 'package:notes/services/location_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class NoteDialog extends StatefulWidget {
-  final Note? note;
+class Note {
+  String? id;
+  final String title;
+  final String description;
+  String? imageUrl;
+  double? latitude;
+  double? longitude;
+  Timestamp? createdAt;
+  Timestamp? updatedAt;
 
-  const NoteDialog({super.key, this.note});
-
-  @override
-  State<NoteDialog> createState() => _NoteDialogState();
-}
-
-class _NoteDialogState extends State<NoteDialog> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  File? _imageFile;
-  Position? _currentPosition;
-  // String? _currentAddress;
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.note != null) {
-      _titleController.text = widget.note!.title;
-      _descriptionController.text = widget.note!.description;
-    }
-  }
-
-  Future<void> _pickImage() async {
-    final pickedFile =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      setState(() {
-        _imageFile = File(pickedFile.path);
-      });
-    }
-  }
-
-  Future<void> _pickLocation() async {
-    final currentPosition = await LocationService.getCurrentPosition();
-    // final currentAddress = await LocationService.getAddressFromLatLng(_currentPosition!);
-    setState(() {
-      _currentPosition = currentPosition;
-      // _currentAddress = currentAddress;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text(widget.note == null ? 'Add Notes' : 'Update Notes'),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Title: ',
-            textAlign: TextAlign.start,
-          ),
-          TextField(
-            controller: _titleController,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              'Description: ',
-            ),
-          ),
-          TextField(
-            controller: _descriptionController,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text('Image: '),
-          ),
-          Expanded(
-              child: _imageFile != null
-                  ? Image.file(_imageFile!, fit: BoxFit.cover)
-                  : (widget.note?.imageUrl != null &&
-                          Uri.parse(widget.note!.imageUrl!).isAbsolute
-                      ? Image.network(widget.note!.imageUrl!, fit: BoxFit.cover)
-                      : Container())),
-          TextButton(
-            onPressed: _pickImage,
-            child: const Text('Pick Image'),
-          ),
-          TextButton(
-            onPressed: _pickLocation,
-            child: const Text('Get Current Location'),
-          ),
-          Text('LAT: ${_currentPosition?.latitude ?? ""}'),
-          Text('LNG: ${_currentPosition?.longitude ?? ""}'),
-          // Text('ADDRESS: ${_currentAddress ?? ""}'),
-        ],
-      ),
-      actions: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('Cancel'),
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            String? imageUrl;
-            if (_imageFile != null) {
-              imageUrl = await NoteService.uploadImage(_imageFile!);
-            } else {
-              imageUrl = widget.note?.imageUrl;
-            }
-            Note note = Note(
-              id: widget.note?.id,
-              title: _titleController.text,
-              description: _descriptionController.text,
-              imageUrl: imageUrl,
-              latitude: _currentPosition?.latitude,
-              longitude: _currentPosition?.longitude,
-              createdAt: widget.note?.createdAt,
-            );
-
-            if (widget.note == null) {
-              NoteService.addNote(note)
-                  .whenComplete(() => Navigator.of(context).pop());
-            } else {
-              NoteService.updateNote(note)
-                  .whenComplete(() => Navigator.of(context).pop());
-            }
-          },
-          child: Text(widget.note == null ? 'Add' : 'Update'),
-        ),
-      ],
+  Note({
+    this.id,
+    required this.title,
+    required this.description,
+    this.imageUrl,
+    this.latitude,
+    this.longitude,
+    this.createdAt,
+    this.updatedAt,
+  });
+  factory Note.fromDocument(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Note(
+      id: doc.id,
+      title: data['title'],
+      description: data['description'],
+      imageUrl: data['image_url'],
+      latitude: data['latitude'] as double,
+      longitude: data['longitude'] as double,
+      createdAt: data['created_at'] as Timestamp,
+      updatedAt: data['updated_at'] as Timestamp,
     );
+  }
+  Map<String, dynamic> toDocument() {
+    return {
+      'title': title,
+      'description': description,
+      'image_url': imageUrl,
+      'latitude': latitude,
+      'longitude': longitude,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+    };
   }
 }
--- notes/lib/note_service.dart
+++ notes/lib/note_service.dart
@@ -1,14 +1,33 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:notes/models/note.dart';
+import 'package:path/path.dart' as path;
 
 class NoteService {
   static final FirebaseFirestore _database = FirebaseFirestore.instance;
   static final CollectionReference _notesCollection =
       _database.collection('notes');
+  static final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  static Future<String?> uploadImage(File imageFile) async {
+    try {
+      String fileName = path.basename(imageFile.path);
+      Reference ref = _storage.ref().child('images/$fileName');
+      UploadTask uploadTask = ref.putFile(imageFile);
+      TaskSnapshot taskSnapshot = await uploadTask;
+      String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      return null;
+    }
+  }
 
   static Future<void> addNote(Note note) async {
     Map<String, dynamic> newNote = {
       'title': note.title,
       'description': note.description,
+      'image_url': note.imageUrl,
       'created_at': FieldValue.serverTimestamp(),
       'updated_at': FieldValue.serverTimestamp(),
     };
@@ -19,6 +38,7 @@ class NoteService {
     Map<String, dynamic> updatedNote = {
       'title': note.title,
       'description': note.description,
+      'image_url': note.imageUrl,
       'created_at': note.createdAt,
       'updated_at': FieldValue.serverTimestamp(),
     };
@@ -42,6 +62,7 @@ class NoteService {
           id: doc.id,
           title: data['title'],
           description: data['description'],
+          imageUrl: data['image_url'],
           createdAt: data['created_at'] != null
               ? data['created_at'] as Timestamp
               : null,
